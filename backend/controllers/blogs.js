2a817a1ef7b827c275eb05006164f3c63fb64f0b
vilequarter <44734839+vilequarter@users.noreply.github.com>
2024-05-07 16:29:28 +0300

    exercises 4.13-4.14

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,27 +1,65 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
-  response.json(blogs);
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  } catch(exception) {
+    next(exception);
+  };
 });
 
-blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes || 0,
-  });
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes || 0,
+    });
+  
+    if(blog.title === undefined || blog.author === undefined){
+      return response.status(400).send({ error: 'Bad Request' });
+    }
+  
+    const result = await blog.save();
+    response.status(201).json(result);
+  } catch(exception) {
+    next(exception);
+  };
+});
 
-  console.log(blog.title);
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findByIdAndDelete(request.params.id);
+    response.status(204).end();
+  } catch(exception) {
+    next(exception);
+  };
+});
 
-  if(blog.title === undefined || blog.author === undefined){
-    return response.status(400).send({ error: 'Bad Request' });
-  }
-
-  const result = await blog.save();
-  response.status(201).json(result);
+blogsRouter.put('/:id', async (request, response, next) => {
+  try {
+    const blog = {
+      id: request.body.id,
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes,
+    };
+    const result = await Blog.findByIdAndUpdate(
+      request.params.id,
+      blog,
+      { new: true, context: 'query' },
+    );
+    if(result === null){
+      response.status(404).end();
+    }
+    response.status(201).end();
+  } catch(exception) {
+    next(exception);
+  };
 });
 
 module.exports = blogsRouter;
--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -4,35 +4,23 @@ const {
 const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const helper = require('./test_helper');
 const app = require('../app');
 const Blog = require('../models/blog');
+const blog = require('../models/blog');
 
 const api = supertest(app);
 
-const initialBlogs = [
-  {
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-  },
-  {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  },
-];
+beforeEach(async () => {
+  await Blog.deleteMany({});
 
-describe.only('api test', () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({});
-    let blogObject = new Blog(initialBlogs[0]);
-    await blogObject.save();
-    blogObject = new Blog(initialBlogs[1]);
-    await blogObject.save();
-  });
+  const blogObjects = helper.initialBlogs
+    .map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
 
+describe('general api test', () => {
   test('notes are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -43,7 +31,7 @@ describe.only('api test', () => {
   test('there are two blogs', async () => {
     const response = await api.get('/api/blogs');
 
-    assert.strictEqual(response.body.length, initialBlogs.length);
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
   });
 
   test('first blog includes react patterns', async () => {
@@ -60,6 +48,9 @@ describe.only('api test', () => {
     assert(contents.includes('id'));
     assert.strictEqual(contents.includes('_id'), false);
   });
+});
+
+describe('POST requests', () => {
 
   test('making a POST request adds a blog to the database', async () => {
     const newBlog = {
@@ -75,10 +66,10 @@ describe.only('api test', () => {
       .expect(201)
       .expect('Content-Type', /application\/json/);
     
-    const response = await api.get('/api/blogs');
-    const contents = JSON.stringify(response.body[2]);
+    const blogsAtEnd = await helper.blogsInDb();
+    const contents = JSON.stringify(blogsAtEnd[2]);
 
-    assert.strictEqual(response.body.length, initialBlogs.length + 1);
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1);
     assert(contents.includes('This is only a test'));
   });
 
@@ -95,13 +86,13 @@ describe.only('api test', () => {
       .expect(201)
       .expect('Content-Type', /application\/json/);
     
-    const response = await api.get('/api/blogs');
-    const likes = response.body[2].likes;
+    const blogsAtEnd = await helper.blogsInDb();
+    const likes = blogsAtEnd[2].likes;
 
     assert.strictEqual(likes, 0);
   });
 
-  test.only('missing title in new blog request returns 400 error', async () => {
+  test('missing title in new blog request returns 400 error', async () => {
     const newBlog = {
       author: 'Ms. Mystery',
       url: 'www.nobodyknows.org',
@@ -112,9 +103,13 @@ describe.only('api test', () => {
       .post('/api/blogs')
       .send(newBlog)
       .expect(400);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
   });
 
-  test.only('missing author in new blog request returns 400 error', async () => {
+  test('missing author in new blog request returns 400 error', async () => {
     const newBlog = {
       title: 'Super clean initiation',
       url: 'www.nobody.ghost',
@@ -125,6 +120,79 @@ describe.only('api test', () => {
       .post('/api/blogs')
       .send(newBlog)
       .expect(400);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+});
+
+describe('DELETE requests', () => {
+  test('making a DELETE request on an id removes the blog', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+    
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+    const deletedTitle = JSON.stringify(blogToDelete.title);
+    assert(!JSON.stringify(blogsAtEnd).includes(deletedTitle));
+  });
+
+  test('making a DELETE request on an invalid id returns a 400 error', async () => {
+    await api
+      .delete('/api/blogs/1234')
+      .expect(400);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+
+  test('making a DELETE request on a nonexistent id returns a 204 error', async () => {
+    const nonExistingId = await helper.nonExistingId();
+    await api
+      .delete(`/api/blogs/${nonExistingId}`)
+      .expect(204);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length);
+  });
+});
+
+describe('UPDATE requests', () => {
+  test('making an UPDATE request on an id changes the info', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
+    const updatedBlog = {
+      ...blogToUpdate,
+      likes: blogToUpdate.likes + 1,
+    };
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(201);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    const updatedInDb = blogsAtEnd[0];
+    assert.strictEqual(blogToUpdate.likes + 1, updatedInDb.likes);
+    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
+  });
+
+  test('making an UPDATE request on an invalid id returns a 404 error', async () => {
+    const nonexistentBlogId = await helper.nonExistingId();
+    const blogsAtStart = await helper.blogsInDb();
+
+    await api
+      .put(`/api/blogs/${nonexistentBlogId}`)
+      .send({ likes: 0 })
+      .expect(404);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
   });
 });
 
