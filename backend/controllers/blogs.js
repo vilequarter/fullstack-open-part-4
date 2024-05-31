43ec6394b1fb4ffceabb8a92f99b31f40a9fceca
vilequarter <44734839+vilequarter@users.noreply.github.com>
2024-05-31 09:22:23 +0300

    exercise 4.20

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization');
-  if(authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '');
-  };
-  return null;
-};
-
 blogsRouter.get('/', async (request, response, next) => {
   try {
     const blogs = await Blog
@@ -23,7 +15,7 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   try {
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
     if(!decodedToken.id) {
       return response.status(401).json({ error: 'token invalid' });
     };
